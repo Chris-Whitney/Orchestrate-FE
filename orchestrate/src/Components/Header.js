48badf8611de90c8024826837fbb8428e9ca676d
mavie1322 <babimbilyoness@gmail.com>
2022-02-23 16:39:25 +0000

    add the UI packages but unsuccessful to implement it, create the Header component and style it and incorporated to the App.js file

---
+++ orchestrate/src/Components/Header.js
@@ -0,0 +1,23 @@
+import "../Styling/Header.css";
+import { Link } from "react-router-dom";
+
+export function Header() {
+  return (
+    <div className='header'>
+      <div className='title'>Orchestrate Logo</div>
+      <nav className='nav-container'>
+        <Link to='/home'>
+          <button className='nav-link'>Home</button>
+        </Link>
+        <Link to='/profile'>
+          <button className='nav-link'>Profile</button>
+        </Link>
+        <div className='status'>
+          <Link to='/login'>
+            <button className='nav-link'>Log out</button>
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+}
