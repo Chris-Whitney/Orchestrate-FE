7c3df2a6ffecd43cd6f914e94ce40ca95f946cd9
stanleyh1 <hannahstanley63@gmail.com>
2022-02-23 17:03:03 +0000

    creates post new user function

--- orchestrate/src/Utils/api.js
+++ orchestrate/src/Utils/api.js
@@ -19,3 +19,32 @@ export const login = (username, password) => {
       console.log(err)
   })
 };
+
+export const postNewUser = (user) => {
+  console.log(user)
+  return orchestrateApi.post('api/users', {
+    "name": {
+      "first": user.firstname,
+      "last": "Stevenson"
+    },
+    "location": {
+      "postcode": "m1 09s",
+      "city": "Manchester",
+      "country": "England"
+    },
+    "avatar_url": "https://avatars.dicebear.com/api/adventurer/erge.svg",
+    "username": user.username,
+    "email": "steve@gmail.com",
+    "instruments": [
+      "Harp"
+    ],
+    "password": user.password
+  
+}).then((res) => {
+  console.log(res.data)
+  return res.data
+})
+.catch((err) => {
+  console.log(err)
+})
+};
